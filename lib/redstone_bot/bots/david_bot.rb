9f2d17a16ecd8b24ca6d9092c4ef7d63d2e98534
David Grayson <davidegrayson@gmail.com>
2012-07-15 01:24:52 -0700

    removed tmphax in david_bot

--- lib/redstone_bot/bots/david_bot.rb
+++ lib/redstone_bot/bots/david_bot.rb
@@ -8,13 +8,7 @@ module RedstoneBot
     def setup
       standard_setup
       
-      previous_closest = nil
       @body.on_position_update do
-        closest = @entity_tracker.closest_entity
-        if closest != previous_closest
-          @client.chat "looking at #{closest.inspect} d=%.4f" % [(closest.position - @body.position).magnitude]
-          previous_closest = closest
-        end
         @body.look_at @entity_tracker.closest_entity      
       end
       
