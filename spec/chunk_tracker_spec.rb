49bf6309169f472e43c98310b65f20c97e0aaa71
David Grayson <davidegrayson@gmail.com>
2012-07-22 13:40:26 -0700

    chunk_tracker_spec: Changed it to be a better test by moving the chunk to x=32

--- spec/chunk_tracker_spec.rb
+++ spec/chunk_tracker_spec.rb
@@ -4,44 +4,46 @@ require 'packet_spec'   # contains helper methods for constructing packet
 require 'zlib'
 
 testdata1 = RedstoneBot::Packet::ChunkData.new
-testdata1.instance_variable_set :@x, 0
+testdata1.instance_variable_set :@x, 2
 testdata1.instance_variable_set :@z, 1
 testdata1.instance_variable_set :@primary_bit_map, 1   # only set the y=0..15 section
-data = ""
-data << "\x3C" * 256   # y = 0 is all farmland
-data << "\x3B" * 256   # y = 1 is all wheat
-data = data.ljust(16*256, "\x00")   # the rest of the block types are air
-data << "\x00" * 128   # y = 0 no metadata
-data << "\x55" * 128   # y = 1 : All crops are 5 tall (7 is the max)
-data = data.ljust(16*256 + 16*128, "\x00")   # the rest of the metadata is 0
+bt = ""
+bt << "\x3C" * 256   # y = 0 is all farmland
+bt << "\x3B" * 256   # y = 1 is all wheat
+bt = bt.ljust(16*256, "\x00")   # the rest of the block types are air
+metadata = ""
+metadata << "\x00" * 128   # y = 0 no metadata
+metadata << "\x55" * 128   # y = 1 : All crops are 5 tall (7 is the max)
+metadata = metadata.ljust(16*128, "\x00")   # the rest of the metadata is 0
+data = bt + metadata
 testdata1.instance_variable_set :@compressed_data, Zlib::Deflate.deflate(data)
 
 describe RedstoneBot::Chunk do
   before do
-    @chunk = RedstoneBot::Chunk.new([0,16])    
+    @chunk = RedstoneBot::Chunk.new([32,16])    
     @chunk.apply_change testdata1
   end
   
   it "should report farmland at y = 0" do
-    @chunk.block_type_id([3,0,17]).should == RedstoneBot::BlockType::Farmland.id
+    @chunk.block_type_id([35,0,17]).should == RedstoneBot::BlockType::Farmland.id
   end
   
   it "should report wheat at y = 1" do
-    @chunk.block_type_id([10,1,20]).should == RedstoneBot::BlockType::Wheat.id
+    @chunk.block_type_id([42,1,20]).should == RedstoneBot::BlockType::Wheat.id
     
     @chunk.block_type_raw_yslice(1).should == "\x3B" * 256
   end
   
   it "should report metadata=5 at y=1" do
-    @chunk.block_metadata([10,1,20]).should == 5
+    @chunk.block_metadata([42,1,20]).should == 5
   end
   
   it "can change individual block type and metadata" do
-    @chunk.set_block_type_and_metadata([10,1,20], RedstoneBot::BlockType::Wool.id, 6)
-    @chunk.block_metadata([10,1,20]).should == 6
-    @chunk.block_type_id([10,1,20]).should == RedstoneBot::BlockType::Wool.id
-    @chunk.block_metadata([11,1,20]).should == 5
-    @chunk.block_type_id([11,1,20]).should == RedstoneBot::BlockType::Wheat.id
+    @chunk.set_block_type_and_metadata([42,1,20], RedstoneBot::BlockType::Wool.id, 6)
+    @chunk.block_metadata([42,1,20]).should == 6
+    @chunk.block_type_id([42,1,20]).should == RedstoneBot::BlockType::Wool.id
+    @chunk.block_metadata([43,1,20]).should == 5
+    @chunk.block_type_id([43,1,20]).should == RedstoneBot::BlockType::Wheat.id
   end
 end
 
@@ -54,19 +56,19 @@ describe RedstoneBot::ChunkTracker do
   end
   
   it "should report farmland at y = 0" do
-    @chunk_tracker.block_type([4,0,18]).should == RedstoneBot::BlockType::Farmland
+    @chunk_tracker.block_type([36,0,18]).should == RedstoneBot::BlockType::Farmland
   end
 
   it "should report wheat at y = 1" do
-    @chunk_tracker.block_type(RedstoneBot::Coords[10,1,20]).should == RedstoneBot::BlockType::Wheat
+    @chunk_tracker.block_type(RedstoneBot::Coords[42,1,20]).should == RedstoneBot::BlockType::Wheat
   end
   
   it "should report metadata at y = 1" do
-    @chunk_tracker.block_metadata([10,1,20]).should == 5
+    @chunk_tracker.block_metadata([42,1,20]).should == 5
   end
   
   it "handles block changes" do
-    coords = [10,1,21]
+    coords = [42,1,21]
     @client << RedstoneBot::Packet::BlockChange.create(coords, RedstoneBot::BlockType::Wool.id, 6)
     @chunk_tracker.block_type(coords).should == RedstoneBot::BlockType::Wool
     @chunk_tracker.block_metadata(coords).should == 6    
@@ -74,18 +76,18 @@ describe RedstoneBot::ChunkTracker do
   
   it "handles multi-block changes" do
     @chunk_tracker.chunks.size.should == 1
-    @chunk_tracker.chunks[[0,16]].instance_variable_get(:@metadata)[0].size.should >= 2048
+    @chunk_tracker.chunks[[32,16]].instance_variable_get(:@metadata)[0].size.should >= 2048
     
     @client << RedstoneBot::Packet::MultiBlockChange.create([
-      [[10,1,23], RedstoneBot::BlockType::Piston.id, 0],
-      [[10,2,23], RedstoneBot::BlockType::Piston.id, 1],
-      [[10,3,23], RedstoneBot::BlockType::Piston.id, 2],
-      [[10,4,23], RedstoneBot::BlockType::Piston.id, 3]
+      [[42,1,23], RedstoneBot::BlockType::Piston.id, 0],
+      [[42,2,23], RedstoneBot::BlockType::Piston.id, 1],
+      [[42,3,23], RedstoneBot::BlockType::Piston.id, 2],
+      [[42,4,23], RedstoneBot::BlockType::Piston.id, 3]
     ])
     
     (0..3).each do |i|
-      @chunk_tracker.block_type([10, 1+i, 23]).should == RedstoneBot::BlockType::Piston
-      @chunk_tracker.block_metadata([10, 1+i, 23]).should == i
+      @chunk_tracker.block_type([42, 1+i, 23]).should == RedstoneBot::BlockType::Piston
+      @chunk_tracker.block_metadata([42, 1+i, 23]).should == i
     end
   end
   
@@ -99,18 +101,18 @@ describe RedstoneBot::ChunkTracker do
     
     it "reports chunk allocation" do
       p = RedstoneBot::Packet::ChunkAllocation.create(testdata1.x, testdata1.z, true)
-      @receiver.should_receive(:info).with([0,16], p)
+      @receiver.should_receive(:info).with([32,16], p)
       @client << p
     end
     
     it "reports chunk changes" do
-      @receiver.should_receive(:info).with([0,16], testdata1)
+      @receiver.should_receive(:info).with([32,16], testdata1)
       @client << testdata1
     end
     
     it "reports chunk deallocation" do
       p = RedstoneBot::Packet::ChunkAllocation.create(testdata1.x, testdata1.z, false)
-      @receiver.should_receive(:info).with([0,16], p)
+      @receiver.should_receive(:info).with([32,16], p)
       @client << p
     end
 
