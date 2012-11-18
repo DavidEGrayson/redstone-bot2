caa659d25f9f13d268bc21a179ea0ff18efabb96
David Grayson <davidegrayson@gmail.com>
2012-11-18 10:13:10 -0800

    Got closer to being able to test the falling, but now I have a problem because of the way we are organizing the body's regular update thread.  It might need to be abstracted a bit.

--- spec/falling_spec.rb
+++ spec/falling_spec.rb
@@ -7,15 +7,12 @@ describe RedstoneBot::Falling do
     b
   end
 
-  let :client do
-    bot.client
-  end
-  
   before do
-    client << RedstoneBot::Packet::PlayerPositionAndLook.new(0, 70, 0, 70+1.62, 0, 0, false)    
+    bot.client << RedstoneBot::Packet::PlayerPositionAndLook.new(0, 70, 0, 70+1.62, 0, 0, false)    
   end
   
   it "falls" do
-    
+    bot.fall_update
+    bot.body.position.should == nil
   end
 end
--- spec/test_body.rb
+++ spec/test_body.rb
@@ -2,6 +2,9 @@ class TestBody < RedstoneBot::Body
   def start_position_updater
     # In production, we run a thread that regularly updates the position.
     # In tests, we just run body.position_update to simulate the action of that thread.
+    
+    # return some dummy object instead of a thread
+    :position_updater
   end
   
   raise "Remove the method below" unless instance_method(:announce_received_position)
