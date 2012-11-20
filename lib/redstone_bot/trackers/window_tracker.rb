0362690623915b79c752d1b3d9764e2e7e766b73
David Grayson <davidegrayson@gmail.com>
2012-11-19 23:51:26 -0800

    Progress on WindowTracker.

--- lib/redstone_bot/trackers/window_tracker.rb
+++ lib/redstone_bot/trackers/window_tracker.rb
@@ -2,26 +2,31 @@ require_relative '../packet_printer'
 
 module RedstoneBot
   class WindowTracker
+    attr_reader :window_id
+  
     PrintPacketClasses = [
       Packet::OpenWindow, Packet::CloseWindow, Packet::SetWindowItems, Packet::SetSlot, Packet::UpdateWindowProperty, Packet::ConfirmTransaction
     ]
     
     def initialize(client)
-      @open = false
-    
       # for debugging
       if client
         @packet_printer = PacketPrinter.new(client, PrintPacketClasses)
         client.listen { |p| receive_packet p } if client
       end
     end
-        
+
     def open?
-      @open
+      @window_id ? true : false
     end
     
     def receive_packet(packet)
-      return unless p.respond_to?(:window_id)
+      return unless packet.respond_to?(:window_id)
+      
+      case packet
+      when Packet::OpenWindow
+        @window_id = packet.window_id
+      end
     end
     
     # A handy function for unit testing.
